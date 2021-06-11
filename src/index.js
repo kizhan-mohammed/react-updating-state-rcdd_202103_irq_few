49ab5812cafdde1538e884effd0d26eb8b96bdab
Kizhan Mohammed <kizhan.m.kareem@gmail.com>
2021-06-11 07:18:57 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,11 +1,4 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import ClickityClick from "./components/ClickityClick";
 
-ReactDOM.render(
-  <div>
-    // Mount Components Here
-    <ClickityClick />
-  </div>,
-  document.getElementById('root')
 );
